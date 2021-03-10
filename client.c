5fce3ed0783ebb82950562b24e5e5c7bb4b62cf3
Alex Profir <alex_profir@yahoo.com>
2021-03-10 13:35:24 +0200

    added wiring for client and server

--- client.c
+++ client.c
@@ -1,89 +1,190 @@
-#include <stdio.h>
+#include <sys/types.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <pthread.h>
+#define HOLD_CMD "HLD"
+#define TURN_CMD "TRN"
+#define UPDATE_CMD "UPD"
+#define COUNT_CMD "CNT"
+#define INVALID_CMD "INV"
+#define WIN_CMD "WIN"
+#define LOSE_CMD "LSE"
+#define DRAW_CMD "DRW"
+#define WAIT_CMD "WAT"
+#define START_CMD "SRT"
 
+
+void error(const char *msg)
+{
+    printf("Either the server shut down or the other player disconnected.\nGame over.\n");
+    exit(0);
+}
+
+void recv_msg(int sockfd, char * msg)
+{
+    memset(msg, 0, 4);
+    int n = read(sockfd, msg, 3);
+  
+    if (n < 0 || n != 3)
+        error("ERROR reading message from server socket.");
+}
+
+int recv_int(int sockfd)
+{
+    int msg = 0;
+    int n = read(sockfd, &msg, sizeof(int));
+  
+    if (n < 0 || n != sizeof(int))
+        error("ERROR reading int from server socket");
+
+    return msg;
+}
+
+void WRITE_TO_SERVER(int sockfd, int msg)
+{
+    int n = write(sockfd, &msg, sizeof(int));
+    if (n < 0)
+        error("ERROR writing int to server socket");
+}
+
+int connect_to_server(char * hostname, int portno)
+{
+    struct sockaddr_in ADDRESS_SERVER;
+    struct hostent *server;
+
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  
+    if (sockfd < 0)
+        error("ERROR opening socket for server.");
+  
+    server = gethostbyname(hostname);
+  
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+  
+   memset(&ADDRESS_SERVER, 0, sizeof(ADDRESS_SERVER));
+
+    ADDRESS_SERVER.sin_family = AF_INET;
+    memmove(server->h_addr, &ADDRESS_SERVER.sin_addr.s_addr, server->h_length);
+    ADDRESS_SERVER.sin_port = htons(portno);
+
+    if (connect(sockfd, (struct sockaddr *) &ADDRESS_SERVER, sizeof(ADDRESS_SERVER)) < 0)
+        error("ERROR connecting to server");
+
+    return sockfd;
+}
+
+
+void draw_PLACE_BORAD(char PLACE_BORAD[][3])
+{
+    printf(" %c | %c | %c \n", PLACE_BORAD[0][0], PLACE_BORAD[0][1], PLACE_BORAD[0][2]);
+    printf("-----------\n");
+    printf(" %c | %c | %c \n", PLACE_BORAD[1][0], PLACE_BORAD[1][1], PLACE_BORAD[1][2]);
+    printf("-----------\n");
+    printf(" %c | %c | %c \n", PLACE_BORAD[2][0], PLACE_BORAD[2][1], PLACE_BORAD[2][2]);
+}
+
+void take_turn(int sockfd)
+{
+    char BUFF_STRING[10];
+  
+    while (1) { 
+        printf("Enter 0-8 to make a move, or 9 for number of active players: ");
+        fgets(BUFF_STRING, 10, stdin);
+        int move = BUFF_STRING[0] - '0';
+        if (move <= 9 && move >= 0){
+            printf("\n");
+            
+            WRITE_TO_SERVER(sockfd, move);
+            break;
+        }
+        else
+            printf("\nInvalid input. Try again.\n");
+    }
+}
+
+void UPDATE_GET(int sockfd, char PLACE_BORAD[][3])
+{
+    int player_id = recv_int(sockfd);
+    int move = recv_int(sockfd);
+
+    PLACE_BORAD[move/3][move%3] = player_id ? 'X' : 'O';  
+}
 int main(int argc, char *argv[])
 {
-	if (argc < 3)
-	{
-		fprintf(stderr, "usage %s hostname port\n", argv[0]);
-		exit(0);
-	}
+    if (argc < 3) {
+       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       exit(0);
+    }
 
-	int sockfd = connect_to_server(argv[1], atoi(argv[2]));
+    int sockfd = connect_to_server(argv[1], atoi(argv[2]));
 
-	int id = recv_int(sockfd);
+    int id = recv_int(sockfd);
 
-	char msg[4];
-	char PLACE_BORAD[3][3] = {{' ', ' ', ' '},
-							  {' ', ' ', ' '},
-							  {' ', ' ', ' '}};
 
-	do
-	{
-		recv_msg(sockfd, msg);
-		if (!strcmp(msg, HOLD_CMD))
-			printf("Waiting for a second player...\n");
-	} while (strcmp(msg, START_CMD));
+    char msg[4];
+    char PLACE_BORAD[3][3] = { {' ', ' ', ' '},
+                         {' ', ' ', ' '},
+                         {' ', ' ', ' '} };
 
-	/* The game has begun. */
-	printf("Game on!\n");
-	printf("Your are %c's\n", id ? 'X' : 'O');
+    do {
+        recv_msg(sockfd, msg);
+        if (!strcmp(msg, HOLD_CMD))
+            printf("Waiting for a second player...\n");
+    } while ( strcmp(msg, START_CMD) );
 
-	draw_PLACE_BORAD(PLACE_BORAD);
+    /* The game has begun. */
+    printf("Game on!\n");
+    printf("Your are %c's\n", id ? 'X' : 'O');
 
-	while (1)
-	{
-		recv_msg(sockfd, msg);
+    draw_PLACE_BORAD(PLACE_BORAD);
 
-		if (!strcmp(msg, TURN_CMD))
-		{
-			printf("Your move...\n");
-			take_turn(sockfd);
-		}
-		else if (!strcmp(msg, INVALID_CMD))
-		{ /* Move was invalid. Note that a "TRN" message will always follow an "INV" message, so we will end up at the above case in the next iteration. */
-			printf("That position has already been played. Try again.\n");
-		}
-		else if (!strcmp(msg, COUNT_CMD))
-		{ /* Server is sending the number of active players. Note that a "TRN" message will always follow a "CNT" message. */
-			int num_players = recv_int(sockfd);
-			printf("There are currently %d active players.\n", num_players);
-		}
-		else if (!strcmp(msg, UPDATE_CMD))
-		{ /* Server is sending a game PLACE_BORAD update. */
-			UPDATE_GET(sockfd, PLACE_BORAD);
-			draw_PLACE_BORAD(PLACE_BORAD);
-		}
-		else if (!strcmp(msg, WAIT_CMD))
-		{ /* Wait for other player to take a turn. */
-			printf("Waiting for other players move...\n");
-		}
-		else if (!strcmp(msg, WIN_CMD))
-		{ /* Winner. */
-			printf("You win!\n");
-			break;
-		}
-		else if (!strcmp(msg, LOSE_CMD))
-		{ /* Loser. */
-			printf("You lost.\n");
-			break;
-		}
-		else if (!strcmp(msg, DRAW_CMD))
-		{ /* Game is a draw. */
-			printf("Draw.\n");
-			break;
-		}
-		else // this should never run
-			error("Unknown message.");
-	}
+    while(1) {
+        recv_msg(sockfd, msg);
 
-	printf("Game over.\n");
+        if (!strcmp(msg, TURN_CMD)) {
+            printf("Your move...\n");
+            take_turn(sockfd);
+        }
+        else if (!strcmp(msg, INVALID_CMD)) { /* Move was invalid. Note that a "TRN" message will always follow an "INV" message, so we will end up at the above case in the next iteration. */
+            printf("That position has already been played. Try again.\n");
+        }
+        else if (!strcmp(msg, COUNT_CMD)) { /* Server is sending the number of active players. Note that a "TRN" message will always follow a "CNT" message. */
+            int num_players = recv_int(sockfd);
+            printf("There are currently %d active players.\n", num_players);
+        }
+        else if (!strcmp(msg, UPDATE_CMD)) { /* Server is sending a game PLACE_BORAD update. */
+            UPDATE_GET(sockfd, PLACE_BORAD);
+            draw_PLACE_BORAD(PLACE_BORAD);
+        }
+        else if (!strcmp(msg, WAIT_CMD)) { /* Wait for other player to take a turn. */
+            printf("Waiting for other players move...\n");
+        }
+        else if (!strcmp(msg, WIN_CMD)) { /* Winner. */
+            printf("You win!\n");
+            break;
+        }
+        else if (!strcmp(msg, LOSE_CMD)) { /* Loser. */
+            printf("You lost.\n");
+            break;
+        }
+        else if (!strcmp(msg, DRAW_CMD)) { /* Game is a draw. */
+            printf("Draw.\n");
+            break;
+        }
+        else // this should never run
+            error("Unknown message.");
+    }
+  
+    printf("Game over.\n");
 
-	/* Close server socket and exit. */
-	close(sockfd);
-	return 0;
+    /* Close server socket and exit. */
+    close(sockfd);
+    return 0;
 }
